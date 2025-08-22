56543b17508043b537230ba48931a1a28c333155
agrimaagrawal2 <agrima.agrawal_cs23@gla.ac.in>
2025-08-23 03:01:33 +0530

    Add files via upload

---
+++ Control Flow File/Q18.java
@@ -0,0 +1,11 @@
+package Level1;
+import java.util.*;
+
+public class Q18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        for(int i = 6; i <= 9; i++)
+            System.out.println(num + " * " + i + " = " + (num * i));
+    }
+}
