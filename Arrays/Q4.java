5085de3e39ca250a90ac5f72c1b8cdda48835d66
agrimaagrawal2 <agrima.agrawal_cs23@gla.ac.in>
2025-08-28 14:49:48 +0530

    Add files via upload

---
+++ Arrays/Q4.java
@@ -0,0 +1,30 @@
+package Level1;
+
+import java.util.Scanner;
+
+public class Q4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] numbers = new double[10];
+        double total = 0.0;
+        int index = 0;
+
+        while (true) {
+            System.out.print("Enter number: ");
+            double input = sc.nextDouble();
+            if (input <= 0 || index == 10) {
+                break;
+            }
+            numbers[index] = input;
+            index++;
+        }
+
+        System.out.println("Numbers entered:");
+        for (int i = 0; i < index; i++) {
+            total += numbers[i];
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("Total = " + total);
+    }
+}
